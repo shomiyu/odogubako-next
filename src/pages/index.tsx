cb7f0e25ecf7a0587c2db83d526fe06bd86f4b88
PT1971 <>
2020-08-12 16:16:05 +0900

    🚧index動かない

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,46 @@
 import { NextPage } from "next";
 import * as React from "react";
-import style from "./index.module.scss";
+import Link from "next/link";
+import axios from "axios";
 
-const Home: NextPage = () => {
-  return <h1 className={style.bold}>Hello world!</h1>;
+interface Props {
+  title: String;
+  code: String;
+}
+
+const Home: NextPage<Props> = (data: Props) => {
+  return (
+    <div>
+      <h1>最新の記事</h1>
+      <div>
+        {data.map((data) => (
+          <React.Fragment key={data.id}>
+            <Link href="/blogs/[id]" as={`blogs/${data.id}`}>
+              <a>
+                <h2>{data.title}</h2>
+                <p>{data.code}</p>
+              </a>
+            </Link>
+          </React.Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+Home.getInitialProps = async () => {
+  const key = {
+    headers: { "X-API-KEY": process.env.API_KEY },
+  };
+
+  const res = await axios.get(
+    `https://odogubako.microcms.io/api/v1/coding_html_head`,
+    key
+  );
+
+  const data = await res.data.contents;
+
+  return { data: data };
 };
 
 export default Home;
