ed9453703996e5a084e00979b17f9de54c9df00f
miyuki shoji <0428.miyuki.shoji@gmail.com>
2020-08-18 22:48:58 +0900

    💄Adobeフォント読み込んだ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
-import type { AppProps } from "next/app";
+import React, { useEffect } from "react";
+import { AppProps } from "next/app";
 import "ress";
 import "../../styles.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = (props: AppProps): JSX.Element => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const WebFont = require("webfontloader");
+
+      WebFont.load({
+        typekit: {
+          id: "qfd4irr",
+        },
+      });
+    }
+  }, []);
+
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
