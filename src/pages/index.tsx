96846920a5a92c32d04515e8e0a02f6c411bed96
miyuki_shoji <0428.miyuki.shoji@gmail.com>
2020-11-05 17:41:30 +0900

    💄スタイルの調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,37 +19,39 @@ const Home: NextPage<Props> = (props: Props) => {
 
   return (
     <Layout>
-      <main>
-        <section className={`${style.section} ${style.news}`}>
+      <main className={style.main}>
+        {/* News */}
+        <section className={style.section}>
           <div className={style.section__wrapper}>
-            <h2 className={style.titlePrimary}>
+            <h2 lang="en" className={style.titlePrimary}>
               news release
-              <small className={style.titlePrimary__ja}>お知らせ</small>
+              <small lang="ja" className={style.titlePrimary__ja}>
+                お知らせ
+              </small>
             </h2>
             <div className={style.newsboad}>
               <div className={style.newsboad__inner}>
                 <dl className={style.newsboad__list}>
                   {newsAry.contents.map((item, index) => (
-                    <div key={index}>
-                      <dt className={style.news}>
-                        <time
-                          className={style.news__date}
-                          dateTime={formatDate(new Date(item.date))}
-                        >
+                    <div key={index} className={style.news}>
+                      <dt className={style.news__date}>
+                        <time dateTime={formatDate(new Date(item.date))}>
                           {formatDateDots(new Date(item.date))}
                         </time>
                       </dt>
                       {item.newsContent.map((news, index) => (
-                        <dd key={index}>
+                        <dd key={index} className={style.news__content}>
                           <section>
                             <h3 className={style.news__title}>{news.title}</h3>
                             <p>
                               {news.details}
-                              <Link href={{ pathname: news.url }}>
-                                <a className={style.news__link}>
-                                  {news.linkName}
-                                </a>
-                              </Link>
+                              {news.url && (
+                                <Link href={{ pathname: news.url }}>
+                                  <a className={style.news__link}>
+                                    {news.linkName}
+                                  </a>
+                                </Link>
+                              )}
                             </p>
                           </section>
                         </dd>
@@ -62,40 +64,38 @@ const Home: NextPage<Props> = (props: Props) => {
           </div>
         </section>
 
-        {/* design */}
-        <section>
-          <div className={style.section__wrapper}>
-            {categoriesAry.contents.map((item, index) => (
-              <div key={index}>
-                <h2 className={style.titlePrimary}>
-                  {item.path}
-                  <small className={style.titlePrimary__ja}>{item.title}</small>
-                </h2>
-                <ul className={style.menu}>
-                  {item.children.map((child, index) => (
-                    <li key={index}>
-                      <Link href={{ pathname: child.path }}>
-                        <a>
-                          <section className={style.menu__item}>
-                            <h3 className={style.menu__title}>
-                              {child.path}
-                              <span className={style.menu__titleSub}>
-                                {child.title}
-                              </span>
-                            </h3>
-                            <figure className={style.menu__icon}>
-                              <img src={child.icon.url} alt={child.title} />
-                            </figure>
-                          </section>
-                        </a>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-        </section>
+        {/* Menu */}
+        {categoriesAry.contents.map((item, index) => (
+          <section key={index} className={style.section}>
+            <div className={style.section__wrapper}>
+              <h2 className={style.titlePrimary}>
+                {item.path}
+                <small className={style.titlePrimary__ja}>{item.title}</small>
+              </h2>
+              <ul className={style.menu}>
+                {item.children.map((child, index) => (
+                  <li key={index}>
+                    <Link href={{ pathname: child.path }}>
+                      <a>
+                        <section className={style.menu__item}>
+                          <h3 className={style.menu__title}>
+                            {child.path}
+                            <span className={style.menu__titleSub}>
+                              {child.title}
+                            </span>
+                          </h3>
+                          <figure className={style.menu__icon}>
+                            <img src={child.icon.url} alt={child.title} />
+                          </figure>
+                        </section>
+                      </a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+        ))}
       </main>
     </Layout>
   );
