4184d6b8178d239e4352fcda966f5edf3c0e8a2d
miyuki_shoji <0428.miyuki.shoji@gmail.com>
2020-11-25 21:59:00 +0900

    🎉NewsListコンポーネント作った

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import style from "./index.module.scss";
 import { NextPage, GetStaticProps } from "next";
-import Link from "next/link";
 import * as React from "react";
 import ArrayList from "../../models/ArrayList";
 import News from "../../models/News";
 import Category from "../../models/Category";
 import DevCMS from "./api/DevCMS";
-import { formatDate, formatDateDots } from "../utils/FormatUtils";
 import Footer from "../../components/shared/Footer";
+import NewsList from "../../components/shared/NewsList";
 import CategoryList from "../../components/shared/categoryList";
 
 interface Props {
@@ -21,50 +20,7 @@ const Home: NextPage<Props> = (props: Props) => {
   return (
     <>
       <main className={style.main}>
-        {/* News */}
-        <section className="section">
-          <div className="container">
-            <h2 lang="en" className={style.titlePrimary}>
-              news release
-              <small lang="ja" className={style.titlePrimary__ja}>
-                お知らせ
-              </small>
-            </h2>
-            <div className={style.newsboad}>
-              <div className={style.newsboad__inner}>
-                <dl className={style.newsboad__list}>
-                  {newsAry.contents.map((item, index) => (
-                    <div key={index} className={style.news}>
-                      <dt className={style.news__date}>
-                        <time dateTime={formatDate(new Date(item.date))}>
-                          {formatDateDots(new Date(item.date))}
-                        </time>
-                      </dt>
-                      {item.newsContent.map((news, index) => (
-                        <dd key={index} className={style.news__content}>
-                          <section>
-                            <h3 className={style.news__title}>{news.title}</h3>
-                            <p>
-                              {news.details}
-                              {news.url && (
-                                <Link href={{ pathname: news.url }}>
-                                  <a className={style.news__link}>
-                                    {news.linkName}
-                                  </a>
-                                </Link>
-                              )}
-                            </p>
-                          </section>
-                        </dd>
-                      ))}
-                    </div>
-                  ))}
-                </dl>
-              </div>
-            </div>
-          </div>
-        </section>
-
+        <NewsList newsAry={newsAry} />
         <CategoryList categories={categories} />
       </main>
 
