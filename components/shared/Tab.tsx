6d4cdfc5f2a2c8798d9e00e02af84fb85c0450ce
miyuki_shoji <0428.miyuki.shoji@gmail.com>
2020-12-11 17:33:29 +0900

    🍺カードの繰り返し処理はtabに持たせた

--- components/shared/Tab.tsx
+++ components/shared/Tab.tsx
@@ -15,7 +15,7 @@ interface tabItem {
   path: string;
 }
 
-const Tab = (props: Props) => {
+const Tab: React.FC<Props> = (props: Props) => {
   const { header, body } = props;
 
   const router = useRouter();
@@ -42,19 +42,34 @@ const Tab = (props: Props) => {
         </div>
 
         <div className={style.body}>
-          <div className="p-tab__inner is-active-tabBody">
-            <section>
-              <h2 className={style.title} lang="en">
-                color
-                <small className={style.title__ja} lang="ja">
-                  カラーツール
-                </small>
-              </h2>
-              <div className={style.hogeWrap}>
-                <CardItem />
-              </div>
-            </section>
-          </div>
+          {body.Categories.map((childCategory, index) => (
+            <div className="" key={index}>
+              <section>
+                <h2 className={style.title} lang="en">
+                  {childCategory.enTitle}
+                  <small className={style.title__ja} lang="ja">
+                    {childCategory.jaTitle}
+                  </small>
+                </h2>
+                {/* アイコン */}
+                <figure>
+                  <img
+                    src={childCategory.icon.url}
+                    alt={childCategory.enTitle}
+                  />
+                </figure>
+
+                <div className={style.hogeWrap}>
+                  {childCategory.posts.map((post, postIndex) => (
+                    <div className={style.hoge} key={postIndex}>
+                      <CardItem post={post} />
+                      {/* <CardItem post={childCategory} /> */}
+                    </div>
+                  ))}
+                </div>
+              </section>
+            </div>
+          ))}
         </div>
       </div>
     </>
