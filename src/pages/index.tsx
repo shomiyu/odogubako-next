1830da448323fbcf62092b62630d274535d3ec53
shomiyu <m1011522@gmail.com>
2022-02-13 18:51:06 +0900

    🎉導入テキストを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import ArrayList from "../models/ArrayList";
 import News from "../models/News";
 import DevCMS from "./api/DevCMS";
+import Introduction from "../components/Introduction";
 import NewsList from "../components/NewsList";
 import CategoryList from "../components/CategoryList";
 
@@ -12,9 +13,12 @@ interface Props {
 
 const Home: NextPage<Props> = (props: Props) => {
   const { newsAry } = props;
+  const introductionText =
+    "Webデザインやコーディングなどの作業で使える便利なツールやチートシートなどをカテゴリーに分けて集めました。\nお道具箱を開けばWeb制作に必要なものが全部揃っている、そんなコンセプトで作ったブックマーク集です。";
 
   return (
     <>
+      <Introduction contents={introductionText} />
       <NewsList newsAry={newsAry} />
       <CategoryList />
     </>
